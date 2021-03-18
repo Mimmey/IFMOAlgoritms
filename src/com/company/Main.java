25765b5b7edb16a37cabc11c8e2b476829227cb2
Mimmey <motyolya@gmail.com>
2021-03-18 20:32:58 +0300

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,6 +22,7 @@ public class Main {
 //        for (int i = 0; i < n; i++) {
 //            arrayList.add(i + 1);
 //        }
+        //
 
         for(int i = 1; i <= n; i++)
             arrayList.add(arrayList.size()/2, i);
