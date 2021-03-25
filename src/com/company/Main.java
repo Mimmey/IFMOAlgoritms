48ba2bbb0917a58619015462801bc6fe90398f4e
Mimmey <motyolya@gmail.com>
2021-03-25 15:04:00 +0300

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,18 +12,22 @@ public class Main {
         Scanner scanner = new Scanner(new File("input.txt"));
         BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
-        Stack<Integer> list = new Stack<Integer>();
+
         int n = scanner.nextInt();
+        int[] stack = new int[1000000];
+        int top = 0;
 
         for(int i = 0; i < n; i++){
             char c = scanner.next().charAt(0);
             switch (c) {
                 case '+':
                     int t = scanner.nextInt();
-                    list.push(t);
+                    stack[top] = t;
+                    top++;
                     break;
                 case '-':
-                    writer.write(list.pop() + "\n");
+                    writer.write(stack[top - 1] + "\n");
+                    top--;
                     break;
             }
         }
