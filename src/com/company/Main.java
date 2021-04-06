476cf11f7160d73e77b11a98aa6aa84af2fb0d38
Mimmey <motyolya@gmail.com>
2021-04-06 22:25:44 +0300

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,36 +10,42 @@ public class Main {
 
     //public static ArrayList<Integer> arrayList = new ArrayList<>();
     public static int[] arrayList;
-    public static int k1;
-    public static int k2;
+    public static int k;
 
     public static void qSort(int lhs, int rhs, BufferedWriter writer){
-        int i, j, pivot, temp;
+        int i, j, pivot, temp, amount;
 
-        pivot = arrayList[(lhs + rhs) / 2];
+        amount = (rhs - lhs) / k;
+
+        pivot = arrayList[lhs + amount / 2 * k];
         i = lhs;
         j = rhs;
 
         do{
             while(arrayList[i] < pivot)
-                i++;
+                i += k;
             while(arrayList[j] > pivot)
-                j--;
+                j -= k;
             if(i <= j) {
                 temp = arrayList[i];
                 arrayList[i] = arrayList[j];
                 arrayList[j] = temp;
-                i++;
-                j--;
+                i += k;
+                j -= k;
             }
         }while(j >= i);
 
         if(lhs < j)
-            if (k1 - 1 >= lhs && k1 - 1<= j || k2 - 1 >= lhs && k2 - 1 <= j || lhs >= k1 - 1 && j <= k2- 1)
-                qSort(lhs, j, writer);
+            qSort(lhs, j, writer);
         if(i < rhs)
-            if (k1 - 1 >= i && k1 - 1<= rhs || k2 - 1 >= i && k2 - 1 <= rhs || i >= k1 - 1 && rhs <= k2- 1)
-                qSort(i, rhs, writer);
+            qSort(i, rhs, writer);
+    }
+
+    public static boolean is (int n){
+        for (int i = 1; i < n; i++)
+            if (arrayList[i] < arrayList[i - 1])
+                return false;
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
@@ -47,49 +53,19 @@ public class Main {
         BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
         int n;
+
         n = scanner.nextInt();
-
-        k1 = scanner.nextInt();
-        k2 = scanner.nextInt();
-
-        int a, b, c;
-        a = scanner.nextInt();
-        b = scanner.nextInt();
-        c = scanner.nextInt();
-
-        int t1, t2;
-        t1 = scanner.nextInt();
-        t2 = scanner.nextInt();
+        k = scanner.nextInt();
 
         arrayList = new int[n];
 
-//        arrayList.add(t1);
-//        arrayList.add(t2);
-//
-//        for(int i = 2; i < n; i++)
-//            arrayList.add(a * arrayList.get(arrayList.size() - 2) + b * arrayList.get(arrayList.size() - 1) + c);
-//
-//        qSort(0, n - 1, writer);
-//
-//        for(int i = k1; i <= k2; i++)
-//            writer.write("" + arrayList.get(i - 1) + " ");
+        for(int i = 0; i < n; i++)
+            arrayList[i] = scanner.nextInt();
 
-        arrayList[0] = t1;
-        arrayList[1] = t2;
+        for (int i = 0; i < k; i++)
+            qSort(i, n - 1 - (n - 1 - i) % k, writer);
 
-        for(int i = 2; i < n; i++)
-            arrayList[i] = a * arrayList[i - 2] + b * arrayList[i - 1] + c;
-
-        qSort(0, n - 1, writer);
-
-//        StringBuilder res = new StringBuilder ("");
-//        for (int k = k1; k1 <= k2; k++)
-//            res.append("${a[k]} ");
-//
-//        writer.write(res.substring(0, res.length() - 1));
-
-        for(int i = k1; i <= k2; i++)
-           writer.write("" + arrayList[i - 1] + " ");
+        writer.write("" + (is(n) ? "YES" : "NO"));
 
         scanner.close();
         writer.close();
