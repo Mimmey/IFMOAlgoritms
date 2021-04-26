bdaeb05927b186a89455cd760c2f8040d9295276
Mimmey <motyolya@gmail.com>
2021-04-26 20:40:22 +0300

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void checkLength(Element element, int current){
-        if (element.left != 0){
-            checkLength(elements[element.left],current + 1);
-        }
-
-        if (element.right != 0){
-            checkLength(elements[element.right],current + 1);
-        }
-
-        if (height < current) {
-            height = current;
-        }
-    }
-
-    static class Element{
+    static class Element {
         int key;
         int left;
         int right;
@@ -30,30 +16,70 @@ public class Main {
         }
     }
 
-    static int height = 0;
-    static Element[] elements;
+    public static Element[] elements;
+    public static int cou = 0;
+    public static int all = 0;
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(new File("input.txt"));
         int n = scanner.nextInt();
+
+        all = n;
         elements = new Element[n + 1];
 
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 1; i <= n; i++) {
             int input1 = scanner.nextInt();
             int input2 = scanner.nextInt();
             int input3 = scanner.nextInt();
+
             elements[i] = new Element(input1, input2, input3);
         }
 
-        if(n != 0) {
-            checkLength(elements[1], 1);
+        int m = scanner.nextInt();
+        int[] keys = new int[m];
+
+        for (int i = 0; i < m ; i++) {
+            keys[i] = scanner.nextInt();
         }
 
-        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"))){
-            bufferedWriter.write(height + "");
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"))) {
+            for (int i = 0; i < m ; i++) {
+                whereItIs(1, keys[i]);
+                bufferedWriter.write(all + "\n");
+            }
+        }
+    }
+
+    public static void amount(Element element) {
+        if(element == null)
+            return;
+        cou++;
+
+        amount(elements[element.left]);
+        amount(elements[element.right]);
+    }
+
+    public static void whereItIs(int index, int key) {
+        if (elements[index] == null) {
+            return;
+        }
+
+        if (elements[index].key == key) {
+            amount(elements[index]);
+            elements[index] = null;
+            all -= cou;
+            cou = 0;
+            return;
+        }
+
+        if(key < elements[index].key) {
+            whereItIs(elements[index].left, key);
+        }
+
+        if(key > elements[index].key) {
+            whereItIs(elements[index].right, key);
         }
 
     }
-
 
 }
